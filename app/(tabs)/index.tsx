6b1c2062b91f059d6d286b2b55c731efa2b558dd
Harut-san <harutikv@gmail.com>
2025-11-01 21:16:53 +0100

    chore/card_colors_update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,26 +6,30 @@ export default function Index() {
     <View style={styles.container}>
       <Text style={styles.text}>Hello Harut!</Text>
       <View>
-        <Card label="Wisdom" screen="Wisdom" colors={["#101923", "#3a0000"]} />
+        <Card
+          label="Wisdom"
+          screen="Wisdom"
+          colors={["#10192347", "#3a000044"]}
+        />
         <Card
           label="Conversation Starters"
           screen="ConversationStarters"
-          colors={["#101923", "#00565dff"]}
+          colors={["#10192346", "#00555d3f"]}
         />
         <Card
           label="Mantras"
           screen="Mantras"
-          colors={["#101923", "#6f0066ff"]}
+          colors={["#10192347", "#6f00663a"]}
         />
         <Card
           label="Daily Motivation"
           screen="DailyMotivation"
-          colors={["#101923", "#003a05ff"]}
+          colors={["#10192347", "#003a0540"]}
         />
         <Card
           label="Mindfulness Prompts"
           screen="Mindfulness"
-          colors={["#101923", "#0048acff"]}
+          colors={["#101923", "#0048ac49"]}
         />
       </View>
     </View>
